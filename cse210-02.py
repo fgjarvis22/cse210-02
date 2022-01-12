f631d526c3ff3fa0bc7d7380266d675138974dff
fgjarvis22 <97373697+fgjarvis22@users.noreply.github.com>
2022-01-12 13:20:34 -0700

    Update cse210-02.py
    messing around with  __init__

--- cse210-02.py
+++ cse210-02.py
@@ -1,55 +1,35 @@
 
 import random
 
+class Turn: 
+
+    def __init__(self, name, score):
+        self.score = score
+        self.name = name
+        self.players = []
+
+    def get_score(self):
+        return self.score 
+
 class Game:
 
-    n = 0
-    choice = 0
-    score = 300 
-    lose = 0
+    players = []
 
-    def turn():
-        Game.choice = input("Higher or lower? [h/l]: ")
-        if Game.choice.lower() == 'l':
-            x = random.randint(1,13)
-            print(f"Next card was: {x}")
-            if Game.n > x:
-                Game.score = Game.score + 100 
-            else:
-                Game.score = Game.score - 75
-        elif Game.choice.lower() == 'h':
-            x = random.randint(1,13) 
-            print(f"Next card was: {x}")
-            if Game.n < x:
-                Game.score = Game.score + 100 
-            else:
-                Game.score = Game.score - 75
+    def num_players():
+        x = 1
+        player_count = int(input("How many players? "))
+        while player_count > 0:
+            player_name = input(f"What is player {x}'s name: ")
+            new_player = Turn(player_name,300)
+            Game.players.append(new_player)
+            x +=1 
+            player_count -= 1
 
-    def check_loss():
-        if Game.score == 0:
-            Game.lose == 1
-            print("Your score has reached 0\nYou lose...")
-
-                
+        
     def main():
-        while True:
-            Game.n = random.randint(1,13) 
-            print(f"The card is: {Game.n}")
-            Game.turn()
-            print(f"Your score is: {Game.score}")
-            Game.check_loss()
-            if Game.lose == 1:
-                break
-            else:
-                while True:
-                    again = input("\nPlay again? [y/n]: ")
-                    if again.lower() == 'y':
-                        print()
-                        Game.main()
-                    elif again.lower() == 'n':
-                        break
-                    else:
-                        print("Error! Please choose either 'y' or 'n'")
-                break
+        Game.num_players()
+        print(Game.players[0].score)
+        return
+        
 
 Game.main()
